0e7672cf152be8b9458f0507f2741bc802de072c
Peter Kraker <peter.kraker@mendeley.com>
2016-05-18 20:06:27 +0200

    Added YEAR to the list of partners

--- footer.php
+++ footer.php
@@ -23,6 +23,7 @@
                 <a href="https://archivelab.org/" target="_blank"><img src="./img/partners/internetarchive.png" alt="Internet Archive"></a>
                 <a href="http://know-center.at/" target="_blank"><img src="./img/partners/knowcenter.png" alt="Know Center"></a>
                 <a href="http://okfn.at" target="_blank"><img src="./img/partners/okfn.png" alt="Open Knowledge Austria"></a>
+                <a href="http://www.year-network.com/" target="_blank"><img src="./img/partners/year-logo.png" alt="Open Knowledge Austria"></a>
             </div>
             
              <div id="license">
