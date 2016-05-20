cb9b110cc8e732a956e7d6466e70417369a000a6
Peter Kraker <peter.kraker@mendeley.com>
2016-05-20 22:44:27 +0200

    Switch ET and zika maps

--- discover.php
+++ discover.php
@@ -1,12 +1,1 @@
-<div id="divdiscover">
-    
-            <h2>Discover Maps</h2> 
-<div class="discovermaps">
-<a target="_blank" href="http://openknowledgemaps.org/educational-technology"><img class="hovermaps" src="./img/zika-map.jpg" alt="Educational Technology Map"></a>
-<h3><a target="_blank" href="http://openknowledgemaps.org/educational-technology">Map &quot;Educational Technology&quot;</a></h3>
-</div>
-<div class="discovermaps">
-<a target="_blank" href="http://openknowledgemaps.org/zika"><img class="hovermaps" src="./img/et-map.jpg" alt="Zika Map"></a>
-<h3><a target="_blank" href="http://openknowledgemaps.org/zika">Map &quot;Zika&quot;</a></h3>
-</div>
-</div>
+<div id="divdiscover">                <h2>Discover Maps</h2> <div class="discovermaps"><a target="_blank" href="http://openknowledgemaps.org/educational-technology"><img class="hovermaps" src="./img/et-map.jpg" alt="Educational Technology Map"></a><h3><a target="_blank" href="http://openknowledgemaps.org/educational-technology">Map &quot;Educational Technology&quot;</a></h3></div><div class="discovermaps"><a target="_blank" href="http://openknowledgemaps.org/zika"><img class="hovermaps" src="./img/zika-map.jpg" alt="Zika Map"></a><h3><a target="_blank" href="http://openknowledgemaps.org/zika">Map &quot;Zika&quot;</a></h3></div></div>
