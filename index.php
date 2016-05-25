1118c40b92f79d18d9c8f41d4770934f5f06de3f
Peter Kraker <peter.kraker@mendeley.com>
2016-05-25 21:50:34 +0200

    Added a tweet collection to the feedback slider. Closes #2.

--- index.php
+++ index.php
@@ -68,40 +68,10 @@
 
         <?php include("discover.php") ?>
         
-        <!-- TEST TWITTER CAROUSEL-->
         
-        <div class="bs-example">
-            <h2 id="feedback"> Feedback</h2>
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                <!-- Carousel indicators -->
-                  
-                <!-- Wrapper for carousel items -->
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img src="./img/slide1.png" alt="First Slide">
-                    </div>
-                    <div class="item">
-                        <img src="./img/slide2.png" alt="Second Slide">
-                    </div>
-                    <div class="item">
-                        <img src="./img/slide3.png" alt="Third Slide">
-                    </div>
-                </div>
-                
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol> 
-                <!-- Carousel controls -->
-                <a class="carousel-control left" href="#myCarousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                </a>
-                <a class="carousel-control right" href="#myCarousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
-            </div>
-        </div>
+        <span class="anchor" id="feedback"></span>
+        
+        <?php include("feedback.php") ?>
 
         <span class="anchor" id="newsletter"></span>
 
