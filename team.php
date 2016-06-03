332a934b0659b05f6ed08b3e5068c41873b76a07
Peter Kraker <peter.kraker@mendeley.com>
2016-06-04 01:41:11 +0200

    New CSS class for advisor descriptions

--- team.php
+++ team.php
@@ -110,99 +110,99 @@
         <div class="advisor">
             <a target="_blank" href="http://contentmine.org"><img class="hoverlink" src="./img/advisors/pmr.jpg" alt="Advisor OKM"></a>
             <h3>Peter Murray-Rust</h3> 
-            <p><a target="_blank" href="http://contentmine.org">Founder of ContentMine</a></p>
+            <p class="advisor-desc"><a target="_blank" href="http://contentmine.org">Founder of ContentMine</a></p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="http://know-center.at"><img class="hoverlink" src="./img/advisors/stefanie.jpg" alt="Advisor OKM"></a>
             <h3>Stefanie Lindstaedt</h3>
-            <p>CEO of Know-Center</p>
+            <p class="advisor-desc">CEO of Know-Center</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="http://www.punkish.org"><img class="hoverlink" src="./img/advisors/puneet.jpg" alt="Advisor OKM"></a>
             <h3>Puneet Kishor</h3>
-            <p>Itinerant Open Science and Data Advocate</p>
+            <p class="advisor-desc">Itinerant Open Science and Data Advocate</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="http://homepage.univie.ac.at/katja.mayer/"><img class="hoverlink" src="./img/advisors/katja.jpg" alt="Advisor OKM"></a>
             <h3>Katja Mayer</h3>
-            <p>Researcher and Lecturer at University of Vienna</p>
+            <p class="advisor-desc">Researcher and Lecturer at University of Vienna</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="http://oana.at"><img class="hoverlink" src="./img/advisors/falk.jpg" alt="Advisor OKM"></a>
             <h3>Falk Reckling</h3>
-            <p>Head of Strategy at Austrian Science Fund (FWF)</p>
+            <p class="advisor-desc">Head of Strategy at Austrian Science Fund (FWF)</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="http://www.elisabethlex.info/"><img class="hoverlink" src="./img/advisors/elisabeth.jpg" alt="Advisor OKM"></a>
             <h3>Elisabeth Lex</h3>
-            <p>Assistant Professor at Graz University of Technology</p>
+            <p class="advisor-desc">Assistant Professor at Graz University of Technology</p>
         </div>
         
         
         <div class="advisor">
             <a target="_blank" href="https://twitter.com/Lambo"><img class="hoverlink" src="./img/advisors/lambert.jpg" alt="Advisor OKM"></a>
             <h3>Lambert Heller</h3>
-            <p>Librarian at German National Library of S&T (TIB)</p>
+            <p class="advisor-desc">Librarian at German National Library of S&T (TIB)</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="https://twitter.com/mxphares"><img class="hoverlink" src="./img/advisors/margaux.jpg" alt="Advisor OKM"></a>
             <h3>Margaux Phares</h3>
-            <p>Science Writer at MIT</p>
+            <p class="advisor-desc">Science Writer at MIT</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="https://twitter.com/sdennerlein"><img class="hoverlink" src="./img/advisors/sebastian.jpg" alt="Advisor OKM"></a>
             <h3>Sebastian Dennerlein</h3>
-            <p>PhD Student, Graz University of Technology</p>
+            <p class="advisor-desc">PhD Student, Graz University of Technology</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="http://www.stefankasberger.at/"><img class="hoverlink" src="./img/advisors/stefan.jpg" alt="Advisor OKM"></a>
             <h3>Stefan Kasberger</h3>
-            <p>Board Member at Open Knowledge Austria</p>
+            <p class="advisor-desc">Board Member at Open Knowledge Austria</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="https://twitter.com/sextus_empirico "><img class="hoverlink" src="./img/advisors/robert.jpg" alt="Advisor OKM"></a>
             <h3>Robert Gutounig</h3>
-            <p>Researcher and Lecturer at FH JOANNEUM</p>
+            <p class="advisor-desc">Researcher and Lecturer at FH JOANNEUM</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="http://open-research.ait.ac.at/confluence/display/AORW/Austrian+Open+Research+Wiki"><img class="hoverlink" src="./img/advisors/michaela.jpg" alt="Advisor OKM"></a>
             <h3>Michela Vignoli</h3>
-            <p>Junior Scientist at Austrian Institute of Technology (AIT)</p>
+            <p class="advisor-desc">Junior Scientist at Austrian Institute of Technology (AIT)</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href=""><img class="hoverlink" src="./img/advisors/christian.jpg" alt="Advisor OKM"></a>
             <h3>Christian Kaier</h3>
-            <p>Open Access Office at University of Graz</p>
+            <p class="advisor-desc">Open Access Office at University of Graz</p>
         </div>
        
         
         <div class="advisor">
             <a target="_blank" href="https://forschung.boku.ac.at/fis/suchen.person_uebersicht?sprache_in=en&ansicht_in=&menue_id_in=101&id_in=113247"><img class="hoverlink" src="./img/advisors/florian.jpg" alt="Advisor OKM"></a>
             <h3>Florian Heigl</h3>
-            <p>PhD Student at BOKU Vienna</p>
+            <p class="advisor-desc">PhD Student at BOKU Vienna</p>
         </div>
         
         <div class="advisor">
             <a target="_blank" href="https://twitter.com/ferli90"><img class="hoverlink" src="./img/advisors/andreas.jpg" alt="Advisor OKM"></a>
             <h3>Andreas Ferus</h3>
-            <p>Dept. Director, Library of the Academy of Fine Arts Vienna</p>
+            <p class="advisor-desc">Dept. Director, Library of the Academy of Fine Arts Vienna</p>
         </div>
         
        <div class="advisor">
             <a target="_blank" href="https://online.boku.ac.at/BOKUonline/visitenkarte.show_vcard?pPersonenId=51947CF9F7441FEF&pPersonenGruppe=3"><img class="hoverlink" src="./img/advisors/daniel.jpg" alt="Advisor OKM"></a>
             <h3>Daniel Dörler</h3>
-            <p>PhD Student at BOKU Vienna</p>
+            <p class="advisor-desc">PhD Student at BOKU Vienna</p>
         </div> 
         
         
