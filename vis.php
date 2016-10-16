9b622805f6f13906c394b15c77e997bcf60a12d8
Peter Kraker <opendiscovery@gmx.at>
2016-10-16 19:32:30 +0200

    Fixed visualization title

--- vis.php
+++ vis.php
@@ -5,43 +5,50 @@
     </head>
 
     <body style="margin:0px; padding:0px">
-        
+
         <?php include ("header_search.php"); ?>
         <!--<div id="empty" style="padding-top:70px;"></div>-->
         <div id="visualization" style="padding-top:78px; background-color:white;"></div>
         <?php
-            $credit = "";
-        
-            if(!isset($_GET['service'])) {
-                echo '<script type="text/javascript" src="data-config_plos.js"></script>';
+        $credit = "";
+
+        $title = "";
+
+        if (!isset($_GET['service'])) {
+            echo '<script type="text/javascript" src="data-config_plos.js"></script>';
+            $credit = '<a href="http://github.com/ropensci/rplos" target="_blank">rplos</a>. Content and metadata retrieved from <a href="https://www.plos.org/publications/journals/" target="_blank">Public Library of Science Journals</a>';
+
+            $title = "PLOS";
+        } else {
+            if ($_GET['service'] === "plos") {
                 $credit = '<a href="http://github.com/ropensci/rplos" target="_blank">rplos</a>. Content and metadata retrieved from <a href="https://www.plos.org/publications/journals/" target="_blank">Public Library of Science Journals</a>';
-            } else {
-                if ($_GET['service'] === "plos") {
-                    $credit = '<a href="http://github.com/ropensci/rplos" target="_blank">rplos</a>. Content and metadata retrieved from <a href="https://www.plos.org/publications/journals/" target="_blank">Public Library of Science Journals</a>';
-                    echo '<script type="text/javascript" src="data-config_plos.js"></script>';
-                } else if ($_GET['service'] === "pubmed") {
-                    $credit = '<a href="https://github.com/ropensci/rentrez " target="_blank ">rentrez</a>. All content retrieved from <a href="http://www.ncbi.nlm.nih.gov/pubmed " target="_blank ">PubMed</a>';
-                    echo '<script type="text/javascript" src="data-config_pubmed.js"></script>';
-                } else if ($_GET['service'] === "doaj") {
-                    $credit = '<a href="https://github.com/ropenscilabs/jaod " target="_blank ">jaod</a>. All content retrieved from <a href="http://www.ncbi.nlm.nih.gov/pubmed " target="_blank ">DOAJ</a>.';
-                    echo '<script type="text/javascript" src="data-config_doaj.js"></script>';
-                }
+                echo '<script type="text/javascript" src="data-config_plos.js"></script>';
+                $title = "PLOS";
+            } else if ($_GET['service'] === "pubmed") {
+                $credit = '<a href="https://github.com/ropensci/rentrez " target="_blank ">rentrez</a>. All content retrieved from <a href="http://www.ncbi.nlm.nih.gov/pubmed " target="_blank ">PubMed</a>';
+                echo '<script type="text/javascript" src="data-config_pubmed.js"></script>';
+                $title = "PubMed";
+            } else if ($_GET['service'] === "doaj") {
+                $credit = '<a href="https://github.com/ropenscilabs/jaod " target="_blank ">jaod</a>. All content retrieved from <a href="http://www.ncbi.nlm.nih.gov/pubmed " target="_blank ">DOAJ</a>.';
+                echo '<script type="text/javascript" src="data-config_doaj.js"></script>';
+                $title = "DOAJ";
             }
+        }
         ?>
         <script>
-                var div_height = ($(document).height() < 750)?(750):($(document).height());
-                $("#visualization").css("height", div_height + "px")
-            
-                data_config.server_url = "http://localhost/project-website/search/server/";
-        	data_config.title = <?php echo json_encode($_GET['query']) ?>;
-        	data_config.files = [{
-        		title: <?php echo json_encode($_GET['query']) ?>,
-        		file: <?php echo json_encode($_GET['id']) ?>
-        	}]
+            var div_height = ($(document).height() < 750) ? (750) : ($(document).height());
+            $("#visualization").css("height", div_height + "px")
+
+            data_config.server_url = "http://localhost/project-website/search/server/";
+            data_config.title = '<?php echo 'Overview of <span id="num_articles"></span> ' . $title . ' articles for ' . json_encode($_GET['query']); ?>';
+            data_config.files = [{
+                    title: <?php echo json_encode($_GET['query']) ?>,
+                    file: <?php echo json_encode($_GET['id']) ?>
+                }]
         </script>
         <script type="text/javascript" src="search/dist/bundle.js"></script>
 
-         <div style="background-color: white">Built with <a href="http://github.com/pkraker/Headstart" target="_blank">Headstart</a> and <?php echo $credit ?>
+        <div style="background-color: white">Built with <a href="http://github.com/pkraker/Headstart" target="_blank">Headstart</a> and <?php echo $credit ?>
         </div>
 
-		<?php include('footer.php'); ?>
+        <?php include('footer.php'); ?>
