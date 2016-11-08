9e6413c72a216ebd5849a10c2f27389996be7e48
Peter Kraker <opendiscovery@gmx.at>
2016-11-08 21:15:06 +0100

    Hotfix for #15, include updated bootstrap versions

--- head_headstart.php
+++ head_headstart.php
@@ -3,7 +3,7 @@
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.1/jquery.validate.min.js"></script>
         <link rel="stylesheet" href="lib/jquery-ui.css">
-        <script type="text/javascript" src="lib/bootstrap.js"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="lib/bootstrap-multiselect.js"></script>
         <link rel="stylesheet" href="lib/bootstrap-multiselect.css">
         <link rel="stylesheet" href="options.css">
