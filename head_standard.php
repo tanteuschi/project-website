589716c3fce0146a15e014d4ef76a5110971c612
maxi <maxi@192.168.0.12>
2016-11-17 18:25:45 +0100

    twitter and OG cards

--- head_standard.php
+++ head_standard.php
@@ -1,25 +1,44 @@
-        <title>Open Knowledge Maps - A visual interface to the world's scientific knowledge</title>
+<title>Open Knowledge Maps - A visual interface to the world&#39;s scientific knowledge</title>
 
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" >
-        <meta http-equiv="content-style-type" content="text/css" >
-        <meta http-equiv="content-language" content="de" >
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <meta name="robots" content="follow" >
-        <meta name="revisit-after" content="1 month" >
-        <meta name="distribution" content="global">
-        <meta name="author" content="Peter Kraker" >
-        <meta name="publisher" content="Peter Kraker" >
-        <!--<meta name="copyright" content="Maxi Schramm" >-->
-        <meta name="keywords" content="knowldege visualization" >
-        
-        <meta property="og:image" content="http://openknowledgemaps.org/img/logo-okmaps.png" />
-        <meta property="og:image" content="http://openknowledgemaps.org/img/headstart-search.png" />
-        
-        <meta name="description" content="Open Knowledge Maps - A visual interface to the world's scientific knowledge" >
-        
-        <link rel="stylesheet" href="./main.css">
-        <!--    <link href='http://fonts.googleapis.com/css?family=Lato:300,100,800' rel='stylesheet' type='text/css'>-->
-        <!--    <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans:300' rel='stylesheet' type='text/css'>-->
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:800,300,400' rel='stylesheet' type='text/css'>
-        
-        <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" >
+<meta http-equiv="content-style-type" content="text/css" >
+<meta http-equiv="content-language" content="de" >
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="robots" content="follow" >
+<meta name="revisit-after" content="1 month" >
+<meta name="distribution" content="global">
+<meta name="author" content="Peter Kraker" >
+<meta name="publisher" content="Peter Kraker" >
+<!--<meta name="copyright" content="Maxi Schramm" >-->
+<meta name="keywords" content="knowldege visualization" >
+
+
+
+<meta name="description" content="Open Knowledge Maps - A visual interface to the world&#39;s scientific knowledge" >
+
+<!-- TWITTER CARD -->
+
+<meta name=”twitter:card” content=”Open Knowledge Maps a visual interface to the world&#39;s scientific knowledge”>
+<meta name=”twitter:site” content=”@OK_Maps”>
+<meta name=”twitter:title” content= “Title specifically for Twitter”>
+<meta name=”twitter:description” content=”We are building a visual interface to the world&#39;s scientific knowledge to change the way we discover research. For more updates follow us @OK_Maps”>
+<meta name=”twitter:creator” content=”Open Knowledge Maps”>
+<meta name=”twitter:image:src” content=http://openknowledgemaps.org/img/logo-okmaps.png>
+<meta name=”twitter:domain” content=”http://openknowledgemaps.org”>
+
+
+<!-- OPEN GRAPH OG -->
+<meta property=”og:description” content=”We are building a visual interface to the world&#39;s scientific 
+      knowledge to change the way we discover research. For more updates 
+      follow us on http://twitter.com/OK_Maps”/>
+      <meta property=”og:url” content=”http://openknowledgemaps.org”/>
+<meta property=”og:image” content=”http://openknowledgemaps.org/img/logo-okmaps.png”/>
+<meta property=”og:type” content=”website”/>
+<meta property=”og:site_name” content=”Open Knowledge Maps”/>
+
+      <link rel="stylesheet" href="./main.css">
+<!--    <link href='http://fonts.googleapis.com/css?family=Lato:300,100,800' rel='stylesheet' type='text/css'>-->
+<!--    <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans:300' rel='stylesheet' type='text/css'>-->
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:800,300,400' rel='stylesheet' type='text/css'>
+
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
