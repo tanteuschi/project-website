17aa875437add7473c128903767c7d62792ee27c
maxi <maxi@192.168.0.12>
2016-11-18 17:17:16 +0100

    description, keywords

--- head_standard.php
+++ head_standard.php
@@ -38,14 +38,12 @@
 <meta property="og:description" content="We are building a visual interface to the world&#39;s scientific 
       knowledge to change the way we discover research. For more updates 
       follow us on http://twitter.com/OK_Maps"/>
-      <meta property="og:url" content="http://openknowledgemaps.org"/>
+<meta property="og:url" content="http://openknowledgemaps.org"/>
 <meta property="og:image" content="http://openknowledgemaps.org/img/cardfb.png"/>
 <meta property="og:type" content="website"/>
 <meta property="og:site_name" content="Open Knowledge Maps"/>
 
-      <link rel="stylesheet" href="./main.css">
-<!--    <link href='http://fonts.googleapis.com/css?family=Lato:300,100,800' rel='stylesheet' type='text/css'>-->
-<!--    <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans:300' rel='stylesheet' type='text/css'>-->
+<link rel="stylesheet" href="./main.css">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:800,300,400' rel='stylesheet' type='text/css'>
 
 <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
