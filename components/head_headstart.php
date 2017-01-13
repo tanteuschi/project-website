a2595bf7561350aa557f7bf89b22bd8f1a54de23
Peter Kraker <opendiscovery@gmx.at>
2017-01-13 20:57:11 +0100

    Fixed problem where dropdown menus would not be transformed to multiselect

--- components/head_headstart.php
+++ components/head_headstart.php
@@ -4,7 +4,7 @@
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.1/jquery.validate.min.js"></script>
         <link rel="stylesheet" href="./lib/jquery-ui.css">
         <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="lib/bootstrap-multiselect.js"></script>
+        <script type="text/javascript" src="./lib/bootstrap-multiselect.js"></script>
         <link rel="stylesheet" href="./lib/bootstrap-multiselect.css">
         <link rel="stylesheet" href="./css/options.css">
         
--- js/menu.js
+++ js/menu.js
@@ -1,35 +1,35 @@
-var main = function() {
-  /* Push the body and the nav over by 285px over */
-  $('.icon-menu').click(function() {
-    $('.menu').animate({
-      left: "0px"
-    }, 200);
+var main = function () {
+    /* Push the body and the nav over by 285px over */
+    $('.icon-menu').click(function () {
+        $('.menu').animate({
+            left: "0px"
+        }, 200);
 
-    $('body').animate({
-      left: "285px"
-    }, 200);
-  });
+        $('body').animate({
+            left: "285px"
+        }, 200);
+    });
 
-  /* Then push them back */
-  $('.icon-close').click(function() {
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
+    /* Then push them back */
+    $('.icon-close').click(function () {
+        $('.menu').animate({
+            left: "-285px"
+        }, 200);
 
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
-  
-  $('.menu li').click(function() {
-    $('.menu').animate({
-      left: "-285px"
-    }, 200);
+        $('body').animate({
+            left: "0px"
+        }, 200);
+    });
 
-    $('body').animate({
-      left: "0px"
-    }, 200);
-  });
+    $('.menu li').click(function () {
+        $('.menu').animate({
+            left: "-285px"
+        }, 200);
+
+        $('body').animate({
+            left: "0px"
+        }, 200);
+    });
 };
 
 
@@ -43,9 +43,11 @@ $(document).ready(function () {
         pathname = pathname.substr(0, index);
     }
     $('a').each(function () {
-       var link = $(this).attr('href');
-       if (link.substr(0,1) == "#") {
-           $(this).attr('href', pathname + link);
-       }
+        var link = $(this).attr('href');
+        if (typeof link != "undefined") {
+            if (link.substr(0, 1) == "#") {
+                $(this).attr('href', pathname + link);
+            }
+        }
     });
 })
