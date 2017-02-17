4a4c2a1811816a072612b7bedf90bfff6b9122b1
Peter Kraker <opendiscovery@gmx.at>
2017-02-17 22:26:19 +0100

    Added warning for mobile users

--- config.example.php
+++ config.example.php
@@ -4,6 +4,7 @@
 $SITE_URL = "//localhost/project-website/";
 $HEADSTART_URL = $SITE_URL . "search/";
 $COMPONENTS_PATH = "components/";
+$LIB_PATH = "lib/";
 
 $PIWIK_ENABLED = false;
 $PIWIK_PATH = "piwik/"
--- vis.php
+++ vis.php
@@ -2,18 +2,35 @@
 <!DOCTYPE HTML>
 <html lang="en">
     <head>
-        
-        <?php include ($COMPONENTS_PATH. "head_standard.php"); ?>
-        
+
+        <?php include ($COMPONENTS_PATH . "head_standard.php"); ?>
+
         <script>
             var intro = {
-                title: "What's this?",    
-		body: '<div class="description-headstart" style="max-width: 1000px"><div style="margin: 0 0 30px;"><p class="icon"><img src="./img/top100.png">            </p>             <p class="icon-description">An Open Knowledge Maps visualization presents you with a topical overview for a search term.                It is based on the most relevant papers in the chosen library.                            </p>        </div>        <div style="margin: 0 0 30px;">            <p class="icon"><img src="./img/textsimilarity.png">            </p>            <p class="icon-description">We use text similarity to create the knowledge maps.                The algorithm groups those papers together that have many words in common.            </p>        </div>        <div style="display: block;">            <p class="icon"><img src="./img/headstart-search.png"></p>            <p class="icon-description">The visualization is intended to give you a head start on your             literature search. You can also use Open Knowledge Maps to stay up-to-date - just limit your search to the most recent papers in the search options.            </p>        </div>'
+            title: "What's this?",
+                    body: '<div class="description-headstart" style="max-width: 1000px"><div style="margin: 0 0 30px;"><p class="icon"><img src="./img/top100.png">            </p>             <p class="icon-description">An Open Knowledge Maps visualization presents you with a topical overview for a search term.                It is based on the most relevant papers in the chosen library.                            </p>        </div>        <div style="margin: 0 0 30px;">            <p class="icon"><img src="./img/textsimilarity.png">            </p>            <p class="icon-description">We use text similarity to create the knowledge maps.                The algorithm groups those papers together that have many words in common.            </p>        </div>        <div style="display: block;">            <p class="icon"><img src="./img/headstart-search.png"></p>            <p class="icon-description">The visualization is intended to give you a head start on your             literature search. You can also use Open Knowledge Maps to stay up-to-date - just limit your search to the most recent papers in the search options.            </p>        </div>'
             }
         </script>
     </head>
 
     <body>
+        <?php
+            require_once $LIB_PATH . 'MobileDetect/Mobile_Detect.php';
+            $detect = new Mobile_Detect;
+            if ($detect->isMobile()):
+        ?>
+
+
+
+            <div class="alert alert-warning">
+
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+
+                Open Knowledge Maps isn't optimized for mobile usage yet. We are working on a better mobile experience; in the meantime, you may encounter some rough edges.
+
+            </div>
+
+        <?php endif ?>
 
         <?php include ($COMPONENTS_PATH . "header_search.php"); ?>
         <!--<div id="empty" style="padding-top:70px;"></div>-->
@@ -26,7 +43,7 @@
         if (!isset($_GET['service'])) {
             echo '<script type="text/javascript" src="./js/data-config_plos.js"></script>';
             $credit = '<a href="http://github.com/ropensci/rplos" target="_blank">rplos</a>. Content and metadata retrieved from <a href="https://www.plos.org/publications/journals/" target="_blank">Public Library of Science Journals</a>';
-            
+
             $title = "PLOS";
         } else {
             if ($_GET['service'] === "plos") {
@@ -44,35 +61,35 @@
             }
         }
         ?>
-        
+
         <?php
-            $query = json_encode($_GET['id']);
-            
-            if (isset($_GET['query'])) {
-                $query = json_encode($_GET['query']);
-            }
+        $query = json_encode($_GET['id']);
+
+        if (isset($_GET['query'])) {
+            $query = json_encode($_GET['query']);
+        }
         ?>
-        
+
         <script>
             var div_height = ($(document).height() < 750) ? (750) : ($(document).height());
             $("#visualization").css("height", div_height + "px")
 
-            data_config.server_url = "<?php echo $HEADSTART_URL ?>server/";
+                    data_config.server_url = "<?php echo $HEADSTART_URL ?>server/";
             data_config.intro = intro;
             data_config.title = '<?php echo 'Overview of <span id="num_articles"></span> ' . $title . ' articles for ' . $query; ?>';
             data_config.files = [{
-                    title: <?php echo $query ?>,
+            title: <?php echo $query ?>,
                     file: <?php echo json_encode($_GET['id']) ?>
-                }]
+            }]
         </script>
         <script type="text/javascript" src="<?php echo $HEADSTART_URL ?>dist/headstart.js"></script>
         <script type="text/javascript">
             headstart.start();
         </script>
-        
+
         <div style="text-align: right; margin: 20px 10px 10px 10px">Built with <a href="http://github.com/pkraker/Headstart" target="_blank">Headstart</a> and <?php echo $credit ?>
         </div>
-        
+
         <link rel="stylesheet" href="<?php echo $HEADSTART_URL ?>dist/headstart.css">
         <link rel="stylesheet" href="./css/main.css">
         <?php include($COMPONENTS_PATH . 'footer.php'); ?>
