9c2fa6eb2a1a88ca5ad5d234cdc262eb1a34f6bc
Peter Kraker <opendiscovery@gmx.at>
2017-03-16 23:48:51 +0100

    Moving menu content to own file

---
+++ components/menu.php
@@ -0,0 +1,59 @@
+            <div class="menu">
+
+                <!-- Menu icon -->
+                <div class="icon-close">
+                    <i class="fa fa-times" aria-hidden="true"></i>
+                </div>
+
+                <!-- Menu -->
+                <ul>
+                    <li><a href="index.php#search">Search</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <!--<li><a href="index.php#discover">Discover Maps</a></li>-->
+                    
+                    <li><a href="team.php#team">Team</a></li>
+                    <li><a href="news.php">News</a></li>
+                    <li><a href="#contact">Get in touch</a></li>
+                    <li><a href="index.php#newsletter">Newsletter</a></li>
+                </ul>
+
+            </div>
+
+            <div class="icon-menu">
+                MENU
+            </div>
+            <div class="imglogo"><a href="index.php"><img src="./img/logo-okmaps.png"></a></div>
+            <ul class="description">
+                
+                <li id="logo"><a href="index.php">Open Knowledge Maps</a></li>
+                <li>A visual interface to the world's scientific knowledge</li>
+            </ul>
+
+            <ul class="nav_top">
+                <li>
+                    <a href="index.php#search">Search</a>
+                </li>
+                <li>
+                    <a href="about.php">About</a>
+                </li>
+                <!--<li>
+                    <a href="index.php#discover">Discover Maps</a>
+                </li>-->
+
+                <li>
+                    <a href="team.php#team">Team</a>
+                </li>
+                <li>
+                    <a href="news.php">News</a>
+                </li>
+
+                <li>
+                    <a href="#contact">Get in touch</a>
+                </li>
+                
+                <li>
+                    <a class="newsletter" href="index.php#newsletter">Newsletter</a>
+                </li>
+                
+            </ul>
+
