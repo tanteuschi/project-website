c521ea37d251a77f0677e2390ebf9a1eef1e3670
maxi <maxi@192.168.0.12>
2017-06-06 21:34:27 +0200

    css

--- components/footer.php
+++ components/footer.php
@@ -3,17 +3,18 @@
 <footer class="getintouch">
 
     <span class="anchor" id="contact"></span>
+    
     <div class="info">
         <address><h3>Get in touch:</h3>
-            <a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a><br>
-            <a href="https://twitter.com/ok_maps" target="_blank">twitter.com/OK_Maps</a><br>
-            <a href="https://github.com/OpenKnowledgeMaps" target="_blank">github.com/OpenKnowledgeMaps</a>                
+            <a href="mailto:info@openknowledgemaps.org"><span class="awesome">&#xf003;</span> info@openknowledgemaps.org</a><br>
+            <a href="https://twitter.com/ok_maps" target="_blank"><span class="awesome">&#xf099;</span> twitter.com/OK_Maps</a><br>
+            <a href="https://github.com/OpenKnowledgeMaps" target="_blank"><span class="awesome">&#xf09b;</span> github.com/OpenKnowledgeMaps</a>                
         </address>
+        
         <ul class="nav_bottom">
-
-            <li><a href="imprint.php" href="">Imprint</a></li>
-             <li><a href="faqs.php" href="">FAQ</a></li>
-            <li><a class="seitenanfang" href="#top">back to top</a></li>
+            <li><a href="imprint.php" href=""><span class="awesome">&#xf0c1;</span> Imprint</a></li>
+            <li><a href="faqs.php" href=""><span class="awesome">&#xf05a;</span> FAQ</a></li>
+            <li class="seitenanfang"><a href="#top"><span class="awesome">&#xf106;</span> back to top</a></li>
         </ul>
     </div>
 
