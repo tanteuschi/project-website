f0be36fcab507a3cd21e6534634d76b600e72ad2
tanteuschi <>
2017-07-14 12:22:30 +0200

    core new partner

--- components/partners.php
+++ components/partners.php
@@ -5,13 +5,14 @@
 
         <a href="http://ropensci.org/" target="_blank"><img src="./img/partners/ropensci.png" alt="rOpenSci"></a>
         <a href="http://contentmine.org/" target="_blank"><img src="./img/partners/contentmine.png" alt="Content Mine"></a>
-        <a href="https://base-search.net/" target="_blank"><img src="./img/partners/base.png" alt="Internet Archive"></a>
+        <a href="https://base-search.net/" target="_blank"><img src="./img/partners/base.png" alt="Bielefeld Academic Search Engine"></a>
         <a href="https://archivelab.org/" target="_blank"><img src="./img/partners/internetarchive.png" alt="Internet Archive"></a>
         <a href="http://know-center.at/" target="_blank"><img src="./img/partners/knowcenter.png" alt="Know Center"></a>
         <a href="http://okfn.at" target="_blank"><img src="./img/partners/okfn.png" alt="Open Knowledge Austria"></a>
-        <a href="http://www.year-network.com/" target="_blank"><img src="./img/partners/year-logo.png" alt="Open Knowledge Austria"></a>
+        <a href="http://core.ac.uk/" target="_blank"><img src="./img/partners/core.png" alt="Core"></a>
         <a href="http://hypothes.is" target="_blank"><img src="./img/partners/hypothesis.png" alt="Hypothes.is"></a>
         <a href="http://wikimedia.at" target="_blank"><img src="./img/partners/Wikimedia_Austria.png" alt="Wikimedia Austria"></a>
+        <a href="http://www.year-network.com/" target="_blank"><img src="./img/partners/year-logo.png" alt="YEAR Network"></a>
 
     </div>
 </div>       
