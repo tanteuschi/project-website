d2f50c8601a25ebd1fc73b837cd6815b47f976ae
maxi <maxi@192.168.0.12>
2017-08-29 17:41:59 +0200

    Search section new: first draft

--- components/footer.php
+++ components/footer.php
@@ -27,7 +27,7 @@
         <a class="licenselink" href="http://creativecommons.org/licenses/by/4.0/" target="_blank">Creative Commons Attribution 4.0 International License</a>.
         Our software is open source and hosted on Github. See our repositories at <a class="licenselink" href="https://github.com/OpenKnowledgeMaps" target="_blank">OpenKnowledgeMaps</a>.
     </div>
-    <p style="text-align:center; margin-top: 20px">Open Knowledge Maps is a non-profit organization<br>dedicated to improving the visibility of scientific knowledge for science and society.</p>
+    <p style="text-align:center; margin-top: 20px; font-size: 14px;">Open Knowledge Maps is a non-profit organization<br>dedicated to improving the visibility of scientific knowledge for science and society.</p>
 
 </footer>
 
